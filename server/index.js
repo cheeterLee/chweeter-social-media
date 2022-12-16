dc27a759a8adbb0ff568fd5563dbe64bebafa199
cheeter_Lee <johnson991210@gmail.com>
2022-12-16 16:39:23 +0800

    add dummy data to to mongodb

--- server/index.js
+++ server/index.js
@@ -16,6 +16,10 @@ import { register } from './controllers/auth.js'
 import { createPost } from './controllers/posts.js'
 import { verifyToken } from "./middleware/auth.js"
 
+import User from './models/User.js'
+import Post from './models/Post.js'
+import { users, posts } from './data/index.js'
+
 // Configurations
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -59,4 +63,9 @@ mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true,
 }).then(() => {
     app.listen(PORT, () => console.log(`Connected to server port: ${PORT}`))
+    
+
+    // Insert dummy data into mongodb
+    // User.insertMany(users)
+    // Post.insertMany(posts)
 }).catch((error) => console.log(`${error} did not connetct`))
